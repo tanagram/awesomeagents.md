4b3e3225d2b348f6ace0081c542f9abf32aea867
Feifan Zhou <feifan@me.com>
2025-12-06 16:46:01 -0800

    Add cover background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,15 @@ export default async function Home() {
       <div className="pointer-events-none fixed inset-0 bg-linear-to-r from-[#9ac5be] via-[#8dbdb5] to-[#7eb3aa]"></div>
       <div className="pointer-events-none fixed inset-0 bg-[radial-gradient(ellipse_at_center,transparent_0%,transparent_40%,rgba(0,0,0,0.2)_100%)]"></div>
       <div className="relative grid min-h-screen w-full justify-items-center overflow-hidden">
-        <div className="flex h-screen w-full flex-col gap-6 p-8 md:flex-row md:gap-0 md:overflow-hidden">
-          <div className="flex h-full w-full flex-col gap-6 overflow-x-scroll md:flex-row">
+        <div className="flex h-screen w-full flex-col gap-6 md:flex-row md:gap-0 md:overflow-hidden">
+          <div
+            className="shadow-elevation-high z-10 flex h-full w-fit shrink-0 gap-6 border-4 border-[#ededd5] bg-cover md:flex-row"
+            style={{ backgroundImage: "url('/cover.jpeg')" }}
+          >
             <TitleCard />
-            <ScrollCard>
-              <TableOfContents data={tocData} />
-            </ScrollCard>
+            <TableOfContents data={tocData} />
+          </div>
+          <div className="flex h-full w-full flex-col gap-6 overflow-x-scroll pl-8 md:flex-row">
             {contentEntries.map((entry, index) => (
               <ScrollCard key={`${entry.category}-${entry.name}`}>
                 <ContentCard
